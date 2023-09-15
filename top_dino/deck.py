6d268c5ad8b8792e088ef8c5fb12e1adbcda04f2
willianrizzo <willianrizzoratao10@gmail.com>
2023-09-15 10:13:32 -0300

    randomizando numeros

--- top_dino/deck.py
+++ top_dino/deck.py
@@ -1,3 +1,5 @@
+import random #biblioteca feita para randomizar 
+
 DINO_NAMES = (
     "Alossauro",
     "Pterodatilo",
@@ -10,12 +12,12 @@ DINO_NAMES = (
 def generate_deck():
     dino_cards =[]
 
-    for dino_name in DINO_NAMES:
+    for dino_name in DINO_NAMES: #criando um dino card para cada nome no Dino_Names
         dino_card = {
             "name": dino_name,
-            "strengh":10,
-            "agility": 12.2,
-            "height": 5.22,
+            "strength":random.randint(1,10), #definindo que sera random de 1 a 10
+            "agility": round(random.uniform(0, 10),1 ),
+            "height": round(random.uniform(0, 10), 2), #uniform serve para randomizar floats, round arredonda casas
         }
 
         dino_cards.append(dino_card) #adciona valores nas listas
